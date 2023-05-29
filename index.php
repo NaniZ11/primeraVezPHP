a58c7e5acd7fa70401c1cd4c7257dfedf9fe4c98
Naniz11 <naniz110504@gmail.com>
2023-05-29 17:03:30 -0500

    feat: :sparkles: introduccion php
    php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+  //  $texto = "Mundo";
+
+    //const MENSAJE = "mundo";
+
+//    printf("%s %s", $texto );
+
+declare (strict_types = 1);
+echo 'Hola,mundo!';
+
+
+
+
+
+
+
+
+
+
+?>    
+
+

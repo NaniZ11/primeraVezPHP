12998a7e6fe1a9c6207ff28618bc528671abad38
Naniz11 <naniz110504@gmail.com>
2023-05-29 17:21:59 -0500

    feat: :sparkles: Variables y constantes
    se incrementa el $ y se le asigna un valor y definicion de constantes en php

--- index.php
+++ index.php
@@ -1,11 +1,25 @@
 <?php
 //Aquí va todo el codigo de php
 //echo "texto a imprimir";
-$texto = "Mundo";
-$mensaje = sprintf ("Hola %s", $texto);
-echo $mensaje;
+//$texto = "Mundo";
+//$mensaje = sprintf ("Hola %s", $texto);
+//echo $mensaje;
 //printf(" Hola %s", $texto);
 
+//declarar una variable numérica
+$edad = 25;
+
+//Declararf una variable booleana
+$es_valido = true;
+echo var_dump($nombre);
+
+//Declarar una constante numérica
+define("PI", 3.1416);
+
+//Declarar una constante de texto 
+define("SALUDO", "Hola Mundo");
+//Declarar una constante booleana
+define("ES_VALIDO", true);
 ?>    
 
 

f33f5fc7e2de8690ed1b2454e0cba137ca34399d
Naniz11 <naniz110504@gmail.com>
2023-05-29 17:30:34 -0500

    feat: :sparkles: Tipos de Datos
    vimos, enteros, flotantes, booleanos, arreglos, objetos etc

--- index.php
+++ index.php
@@ -7,19 +7,37 @@
 //printf(" Hola %s", $texto);
 
 //declarar una variable numérica
-$edad = 25;
+    //$edad = 25;
 
 //Declararf una variable booleana
-$es_valido = true;
-echo var_dump($nombre);
+    //$es_valido = true;
+    //echo var_dump($nombre);
 
 //Declarar una constante numérica
-define("PI", 3.1416);
+    //define("PI", 3.1416);
 
 //Declarar una constante de texto 
-define("SALUDO", "Hola Mundo");
+    //define("SALUDO", "Hola Mundo");
 //Declarar una constante booleana
-define("ES_VALIDO", true);
+    //define("ES_VALIDO", true);
+
+//BOOLEAN 
+    //$logueado = true;
+    //var_dump($logueado);
+
+//Enteros 
+    //$numero = 200;
+    //var_dump($numero);
+
+//Floats 
+    //$float = 200.5;
+    //var_dump($float);
+
+//Floats
+    //$nombre = "Juan";
+    //var_dump($nombre);
+    $array = [];
+    var_dump($array);
 ?>    
 
 
